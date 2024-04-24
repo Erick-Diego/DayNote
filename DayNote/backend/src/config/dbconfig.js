0a3623b9c81e5af8543c44a3ca5fa2dc64f108bf
DigitalVoid <165980277+Erick-Diego@users.noreply.github.com>
2024-04-24 15:07:35 -0300

    Update dbconfig.js

--- DayNote/backend/src/config/dbconfig.js
+++ DayNote/backend/src/config/dbconfig.js
@@ -1,7 +1,6 @@
 const mongoose = require('mongoose');
 
-const Dbconfig = 'mongodb+srv://erickdiego515max:SYNW2xOxtmRGjFU4@cluster0.j2xuhyf.mongodb.net/Notes';
+const Dbconfig = 'mongodb+srv://<usuario>:<senha>@cluster0.j2xuhyf.mongodb.net/Notes';
 const con = mongoose.connect(Dbconfig);
 
 module.exports = con;
-
